578bba8facd84d5ca40dd7219da33395e43da06e
Jacob Connelly <jacob.connelly2018@outlook.com>
2023-12-05 17:54:19 -0700

    finish about_keyword_arguments

--- path_to_enlightenment.rb
+++ path_to_enlightenment.rb
@@ -13,7 +13,7 @@ require 'about_nil'
 require 'about_hashes'
 require 'about_methods'
 in_ruby_version("2", "3") do
-  # require 'about_keyword_arguments'
+  require 'about_keyword_arguments'
 end
 require 'about_constants'
 require 'about_regular_expressions'
@@ -33,7 +33,7 @@ require 'about_modules'
 require 'about_scope'
 require 'about_class_methods'
 require 'about_message_passing'
-require 'about_proxy_object_project'
+# require 'about_proxy_object_project'
 # require 'about_to_str'
 in_ruby_version("jruby") do
   require 'about_java_interop'
