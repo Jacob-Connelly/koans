5f27af5847438d6d1b54777b6fe89b14771ef257
Jacob Connelly <jacob.connelly2018@outlook.com>
2023-12-05 19:03:24 -0700

    Finish about_triangle_project_2

--- triangle.rb
+++ triangle.rb
@@ -14,8 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a == b && b == c
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError
+  elsif a == b && b == c
     :equilateral
+  elsif a + b <= c || b + c <= a || c + a <= b
+    raise TriangleError
   elsif a != b && a != c && b != c
     :scalene
   else
