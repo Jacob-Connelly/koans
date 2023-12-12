558d5bdec11f6da6ca5fd074b0e55d6950ff5012
Jacob Connelly <jacob.connelly2018@outlook.com>
2023-12-11 18:20:08 -0700

    Think I finished about_proxy_object_project. Not sure ruby is throwing errors super randomly.

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -15,10 +15,27 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class Proxy
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
   end
 
-  # WRITE CODE HERE
+  attr_accessor :messages
+
+  def method_missing(method_name, *args,  &block)
+    @messages << method_name
+    if args == []
+      @object.__send__(method_name)
+    else
+      @object.__send__(method_name, args[0])
+    end
+  end
+
+  def called?(method_name)
+    messages.include? method_name
+  end
+
+  def number_of_times_called(method_name)
+    messages.count(method_name)
+  end
 end
 
 # The proxy object should pass the following Koan:
